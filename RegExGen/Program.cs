39f2c6240ac943e493888de03f463c6574c0b308
Unknown <rm.hobbel@telfort.nl>
2019-04-24 18:14:17 +0200

    Test and RegExp Seem to work properly

--- RegExGen/Program.cs
+++ RegExGen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,47 @@ namespace RegExGen
         [STAThread]
         static void Main()
         {
+            TestRegExp();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
+        }
+
+        private static RegExp expr1, expr2, expr3, expr4, expr5, a, b, all;
+
+        public static void TestRegExp()
+        {
+            Debug.WriteLine("Start Test");
+           a = new RegExp("a");
+            b = new RegExp("b");
+
+            // expr1: "baa"
+            expr1 = new RegExp("baa");
+            // expr2: "bb"
+            expr2 = new RegExp("bb");
+            // expr3: "baa | baa"
+            expr3 = expr1.or(expr2);
+
+            // all: "(a|b)*"
+            all = (a.or(b)).star();
+
+            // expr4: "(baa | baa)+"
+            expr4 = expr3.plus();
+            // expr5: "(baa | baa)+ (a|b)*"
+            expr5 = expr4.dot(all);
+            testLanguage();
+        }
+
+        public static void testLanguage()
+        {
+            Debug.WriteLine("taal van (baa):\n" + expr1.getLanguage(5));
+            Debug.WriteLine("taal van (bb):\n" + expr2.getLanguage(5));
+            Debug.WriteLine("taal van (baa | bb):\n" + expr3.getLanguage(5));
+
+            Debug.WriteLine("taal van (a|b)*:\n" + all.getLanguage(5));
+            Debug.WriteLine("taal van (baa | bb)+:\n" + expr4.getLanguage(5));
+            Debug.WriteLine("taal van (baa | bb)+ (a|b)*:\n" + expr5.getLanguage(6));
         }
     }
 }
