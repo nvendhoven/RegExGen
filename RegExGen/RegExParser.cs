067f0cee71b5e712eb03bdedf7d60c0c12134df4
Unknown <rm.hobbel@telfort.nl>
2019-06-04 21:31:23 +0200

    Started addin into parser class

--- RegExGen/RegExParser.cs
+++ RegExGen/RegExParser.cs
@@ -8,7 +8,37 @@ namespace RegExGen
 {
     public class RegExParser
     {
+        public static RegExp ParseRegExp(string regExpString)
+        {
+            RegExp regExp;
+            regExpString = regExpString.Trim();
+            //Loop through all characters of a regEx
+            foreach (char c in regExpString)
+            {
+                switch (c)
+                {
+                    //case '(': break;
+                    //case ')': break;
+                    case '+': break;//doPlus
+                    case '*': break;//doStar
+                    case '|': break;//doOr
+                    default:
+                    {
+                        //c is een char
 
+
+                    } break; //itsALetter
+
+                }
+                
+
+
+
+
+
+            }
+            return null;
+        }
 
     }
 }
