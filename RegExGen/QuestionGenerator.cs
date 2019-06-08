adbc14e63c728180af1b030df6207af261c3cd0a
Unknown <rm.hobbel@telfort.nl>
2019-06-08 19:31:52 +0200

    Added Generator Class

---
+++ RegExGen/QuestionGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegExGen
+{
+    class QuestionGenerator
+    {
+        public static void GenerateQuestion()
+        {
+            
+        }
+    }
+}
